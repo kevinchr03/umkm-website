66369ec270bc0ae734864c9a8ec381354f200170
ByteDevX <git@kevinchr.com>
2025-05-30 01:19:49 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import EditUMKM from '../views/Admin/EditUMKM.vue'
 import ErrorPage from '../views/ErrorPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: '/',
